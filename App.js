545a74cb411d2aeeb2e84f562647d9d4dc554f0d
Pritesh Zaveri <priteshjhaveri27@gmail.com>
2018-09-09 16:31:04 +0530

    initial commit

---
+++ App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Hello World!</Text>
+        <Text style={styles.subtext}>Welcome to my first App..</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  subtext: {
+    color: "#aaa"
+  }
+});
